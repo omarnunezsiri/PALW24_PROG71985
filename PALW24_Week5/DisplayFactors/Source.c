faff2f4b2e9e1c50deeaac92d262edca4900c379
Omar Nunez <omarnunezsiri@gmail.com>
2024-02-06 17:03:58 -0500

    🔨 Update Week 5 (Tuesday) files

--- PALW24_Week5/DisplayFactors/Source.c
+++ PALW24_Week5/DisplayFactors/Source.c
@@ -9,7 +9,7 @@
 // 1.1			2023-10-03		reviewed
 
 #include <stdio.h>
-
+#include <stdbool.h>
 void displayFactors(int);
 
 int main(void)
@@ -17,15 +17,32 @@ int main(void)
 	int num;
 	printf("Please enter a positive integer: ");
 
-	// how would you implement accepting only positive integers?
 	scanf_s("%d", &num);
-	displayFactors(num);
 
+	// checking if the number is positive
+	bool isPositive = num > 0;
+
+	// if the number is positive, display the factors; else, display an error message
+	if (isPositive)
+	{
+		displayFactors(num);
+	}
+	else 
+	{
+		printf("You did not enter a positive integer. Exiting the program...\n");
+	}
+	
 	return 0;
 }
 
 void displayFactors(int num)
 {
+	/*
+	* Quick Review of For Loops
+	* 1. Initialization
+	* 2. Condition
+	* 3. Increment/Decrement (common section)
+	*/
 	for (int i = 1; i <= num; i++)
 	{
 		if (num % i == 0)
--- PALW24_Week5/FindMin/Source.c
+++ PALW24_Week5/FindMin/Source.c
@@ -17,12 +17,15 @@ int main(void)
 	int firstNum = 350;
 	int secondNum = 15;
 
-	int min = findMin(firstNum, secondNum);
+	int min = findMin(firstNum, secondNum); 
 
 	printf("The min between %d and %d is: %d", firstNum, secondNum, min);
 	return 0;
 }
 
+// what if we wanted to refactor this to find the max as well?
+// 1. we could pass a pointer to reference min and max in the function
+// 2. or we could assume that firstNum will be the min and secondNum will be the max
 int findMin(int firstNum, int secondNum)
 {
 	int min;
