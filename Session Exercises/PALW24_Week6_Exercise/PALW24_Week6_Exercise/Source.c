cefc1dc2074242582f305c3dc324ada1591bb76a
Omar Nunez <omarnunezsiri@gmail.com>
2024-02-12 11:26:17 -0500

    ✨ Add Week 6 (Monday) initial files

---
+++ Session Exercises/PALW24_Week6_Exercise/PALW24_Week6_Exercise/Source.c
@@ -0,0 +1,30 @@
+// PAL Week 6 - Exercise:
+//					Write a C program that uses the hardcoded array of student names and takes their overall GPA from input.
+//					The program then *should* display the names of the students with a GPA of 3.0 or higher.
+// 
+// 
+// Challenge(s): 
+//	- 
+// 
+// PROG71985W24
+// Authors: PAL Attendees
+// 
+// 
+// revision history:
+// 1.0			2024-02-12		initial
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_FULL_NAME 50
+#define MAX_STUDENTS 5
+
+#include <stdio.h>
+
+int main() 
+{
+	// DO NOT MODIFY THIS CODE
+	char studentNames[MAX_STUDENTS][MAX_FULL_NAME] = { "Omar Nunez\0", "Ema Anderson\0", "Valentina Jones\0", "Sophie Nunez\0", "George Fletcher\0" };
+
+	// YOUR CODE GOES HERE
+
+	return 0;
+}
